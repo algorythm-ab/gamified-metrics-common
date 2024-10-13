210be9ee4c12c924104c680774cf4495fc2c02b2
Daniel Wilén <daniel.wilen@algorythm.se>
2024-10-13 10:14:16 +0200

    Adding CreateLogger

---
+++ methods/logger.go
@@ -0,0 +1,48 @@
+package methods
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// Create tailored logger
+func CreateLogger(level string) *zap.Logger {
+	logLevel := zap.InfoLevel
+	encoderCfg := zap.NewProductionEncoderConfig()
+	encoderCfg.TimeKey = "timestamp"
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	// Set debug level
+	switch level {
+	case "debug":
+		logLevel = zap.DebugLevel
+	case "warn":
+		logLevel = zap.WarnLevel
+	case "error":
+		logLevel = zap.ErrorLevel
+	case "panic":
+		logLevel = zap.PanicLevel
+	case "fatal":
+		logLevel = zap.FatalLevel
+	default:
+		logLevel = zap.InfoLevel
+	}
+
+	config := zap.Config{
+		Level:             zap.NewAtomicLevelAt(logLevel),
+		Development:       false,
+		DisableCaller:     false,
+		DisableStacktrace: false,
+		Sampling:          nil,
+		Encoding:          "json",
+		EncoderConfig:     encoderCfg,
+		OutputPaths: []string{
+			"stderr",
+		},
+		ErrorOutputPaths: []string{
+			"stderr",
+		},
+	}
+
+	return zap.Must(config.Build())
+}
